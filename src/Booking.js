344984ae58de5c07d22acbd60399a9928c63dde0
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-09-22 15:28:26 -0500

    Pass first four booking tests

--- src/Booking.js
+++ src/Booking.js
@@ -1,6 +1,8 @@
 class Booking {
   constructor(bookingData) {
-
+    this.id = bookingData.id;
+    this.userID = bookingData.userID;
+    this.date = bookingData.date;
   }
 }
 
--- test/booking-test.js
+++ test/booking-test.js
@@ -14,5 +14,17 @@ describe('Booking', () => {
   it('Should be a function', () => {
     expect(Booking).to.be.a('function');
   });
+  it('Should have value of booking ID', () => {
+    expect(booking1.id).to.equal(bookings[0].id);
+    expect(booking2.id).to.equal("5fwrgu4i7k55hl6t5");
+  });
+  it('Should have the user ID', () => {
+    expect(booking1.userID).to.equal(9);
+    expect(booking2.userID).to.equal(bookings[1].userID);
+  });
+  it('Should have booking date', () => {
+    expect(booking1.date).to.equal(bookings[0].date);
+    expect(booking2.date).to.equal("2020/01/24");
+  });
 
 });
