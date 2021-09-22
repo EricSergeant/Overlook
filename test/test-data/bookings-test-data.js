c2ff96655557b985df63bb4942d5971935a701f8
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-09-22 13:41:50 -0500

    Create test data for each class

---
+++ test/test-data/bookings-test-data.js
@@ -0,0 +1,67 @@
+const bookings = [
+  {
+    "id": "5fwrgu4i7k55hl6sz",
+    "userID": 9,
+    "date": "2020/04/22",
+    "roomNumber": 15,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t5",
+    "userID": 43,
+    "date": "2020/01/24",
+    "roomNumber": 24,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t6",
+    "userID": 13,
+    "date": "2020/01/10",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t7",
+    "userID": 20,
+    "date": "2020/02/16",
+    "roomNumber": 7,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2020/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t9",
+    "userID": 38,
+    "date": "2020/02/14",
+    "roomNumber": 14,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6ta",
+    "userID": 25,
+    "date": "2020/01/11",
+    "roomNumber": 9,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6tb",
+    "userID": 2,
+    "date": "2020/02/06",
+    "roomNumber": 1,
+    "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6tc",
+    "userID": 2,
+    "date": "2020/01/30",
+    "roomNumber": 2,
+    "roomServiceCharges": []
+  }
+];
+
+export default bookings;
