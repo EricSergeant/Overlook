e017f2f7dc58397602520bee422455fe7475bb2a
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-09-22 19:45:41 -0500

    Create basic scripts structure with api calls

--- src/scripts.js
+++ src/scripts.js
@@ -4,8 +4,37 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// An example of how you tell webpack to use an image 
+//(also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import { customerPromise, bookingsPromise, roomsPromise } from './apiCalls';
+
+let customerData, bookingsData, roomsData;
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+
+
+function gatherData() {
+  let apiCustomerData = customerPromise()
+    .then(data => data)
+    .catch(error => console.log(`API customer error: ${error.message}`))
+  let apiBookingsData = bookingsPromise()
+    .then(data => data)
+    .catch(error => console.log(`API booking error: ${error.message}`))
+  let apiRoomsData = roomsPromise()
+    .then(data => data)
+    .catch(error => console.log(`API room error: ${error.message}`))
+  Promise.all([apiCustomerData, apiBookingsData, apiRoomsData])
+    .then(data => initData(data))
+}
+
+function initData(data) {
+  customerData = data[0];
+  bookingsData = data[1];
+  roomsData = data[2];
+  //instantiate the classes with this
+}
+
