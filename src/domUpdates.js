b013d3a561ac5f384d91edc2d63afc11b11e3cd1
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-09-27 18:58:10 -0500

    Remove unused code on scripts and domUpdates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,14 +4,12 @@ const pastBookings = document.getElementById('pastStays');
 const noPastBookings = document.getElementById('noPastStays');
 const upComingStays = document.getElementById('upcomingStays');
 const noUpComingStays = document.getElementById('noUpcomingStays');
-// const availableToBook = document.getElementById('available-rooms');
 const availableRooms = document.getElementById('availableRooms');
 
 const profilePage = document.getElementById('profile-page');
 const findRoom = document.getElementById('new-book-view')
 const logOut = document.getElementById('login-page')
 const navMenu = document.getElementById('allNavButtons')
-
 
 const domUpdates = {
 
@@ -26,8 +24,9 @@ const domUpdates = {
       totalSpent.innerHTML +=
         `<p>Welcome first-time customer ${customer.name}!</p>`
     } else {
-      // eslint-disable-next-line max-len
-      totalSpent.innerHTML += `<p>You've spent $${customer.totalSpent} to date.  Thank you for returning to us!</p>`
+      totalSpent.innerHTML +=
+        `<p>You've spent $${customer.totalSpent} to date.
+        Thank you for returning to us!</p>`
     }
   },
 
--- src/scripts.js
+++ src/scripts.js
@@ -25,7 +25,6 @@ let mm = String(date.getMonth() + 1).padStart(2, '0')
 let yyyy = date.getFullYear()
 date = yyyy + '/' + mm + '/' + dd
 
-// console.log('This is the JavaScript entry file - your code begins here.');
 
 // *** query selectors ***
 const viewRooms = document.getElementById('submit-search');
@@ -53,6 +52,8 @@ bookBtn.addEventListener('click', bookRoom)
 profileBtn.addEventListener('click', domUpdates.profileView)
 bookingBtn.addEventListener('click', domUpdates.bookingView)
 logoutBtn.addEventListener('click', domUpdates.logout)
+
+// *** on login ***
 submitLogin.addEventListener('click', (e) => {
   e.preventDefault();
   username = loginForm.username.value;
@@ -71,32 +72,8 @@ submitLogin.addEventListener('click', (e) => {
 })
 
 
-// *** event handlers ***
-// * on load *
-
+// *** on load ***
 // window.addEventListener('load', gatherData);
-
-// *** login ***
-// function checkLogin(event) {
-//   if (!username.value && !password.value) {
-//     preventDefault(event)
-//     console.log('no login info provided')
-//   } else if (password.value !== "overlook2021") {
-//     preventDefault(event)
-//     console.log('wrong password entered')
-//   } else if (username.value !== "customer50") {
-//     preventDefault(event)
-//     console.log('wrong username entered')
-//   } else {
-//     preventDefault(event)
-//     confirmedUser(username.value)
-//   }
-// }
-
-// function confirmedUser(username) {
-//   usernameID = username.slice(8, 10)
-//   usernameID = Number.parseInt(usernameID)
-// }
 
 // *** data initialization ***
 function gatherData() {
@@ -117,30 +94,18 @@ function initData(data) {
   customerData = data[0];
   bookingsData = data[1];
   roomsData = data[2];
-  // console.log('Here is the promise data from scripts file:', data)
 
-  // * instantiate the classes with this *
-  // instantiateRandomUser();
   initCustomer();
   initRooms();
   initBookings();
   renderUserDisplay();
 }
 
-// * note: not using this function, hardcoded user on following function *
-// function instantiateRandomUser() {
-//   let randomUser = customerData.customers[Math
-//     .round(Math.random() * customerData.customers.length)];
-//   // let randomUser = customerData.customers[5];
-//   // console.log('random user result:', randomUser)
-//   // domUpdates.displayUserName(currentUser);
-// }
-
 function initCustomer() {
   // ** testing version:
-  // customer = new Customer(customerData.customers[5]);
+  customer = new Customer(customerData.customers[11]);
   // ** final, live version:
-  customer = new Customer(customerData.customers[username - 1]);
+  // customer = new Customer(customerData.customers[username - 1]);
   console.log('customer on scripts:', customer)
   customer.createCustomerBookings(bookingsData.bookings)
   customer.createCustomerRooms(roomsData.rooms)
@@ -154,7 +119,6 @@ function initRooms() {
     let newRoom = new Room(room)
     allRooms.push(newRoom)
   })
-  // console.log('roomData', roomsData)
   // console.log('instantiated rooms', allRooms)
   return allRooms;
 }
@@ -198,20 +162,17 @@ function renderUserDisplay() {
 function showAvailableRooms(date, type, customer) {
   event.preventDefault()
   parsedDate = date.split("-").join("/");
-  // console.log('customer entry to f:', customer)
-  // console.log('parsed date:', parsedDate)
-  // console.log('today:', today)
-  // console.log('allbookings:', allBookings)
   if (!date) {
-    // eslint-disable-next-line max-len
-    return domUpdates.displayMessage(dateError, "Please choose a date in order to view available rooms.")
+    return domUpdates.displayMessage(dateError,
+      "Please choose a date in order to view available rooms.")
   } else if (parsedDate < today) {
-    return domUpdates.displayMessage(dateError, "Cannot book rooms in the past. Please pick a valid date.")
+    return domUpdates.displayMessage(dateError,
+      "Cannot book rooms in the past. Please pick a valid date.")
   } else {
     // console.log('customer in show:', customer.
     // filterAvailableRoomsByDate(parsedDate, allBookings))
-    // eslint-disable-next-line max-len
-    domUpdates.displayMessage(dateError, "These are the available rooms for that date:")
+    domUpdates.displayMessage(dateError,
+      "These are the available rooms for that date:")
 
     customer.filterUnavailableRoomsByDate(date, bookingsData.bookings);
     customer.filterRoomsByType(roomsData.rooms, selectType.value)
@@ -222,8 +183,6 @@ function showAvailableRooms(date, type, customer) {
 
 
 function bookRoom() {
-  // console.log('hit it')
-  // let closeButton = bookARoom.closest('button')
   let userIDPost = customer.id;
   let datePost = parsedDate;
   // let roomNumberPost = bookingButton.srcElement.id;
@@ -251,7 +210,6 @@ function postBooking(userID, date, roomNumber) {
 }
 
 function renderPost() {
-  // console.log("ready to render, add re-fetch")
   console.log('posted data', allBookings)
   gatherData();
 }
